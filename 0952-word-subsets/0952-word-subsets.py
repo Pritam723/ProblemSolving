d59d3fe696e29b5b2d3177c087231463b7bdb79d
Pritam723 <pritam.dutta723@gmail.com>
2025-01-14 22:43:50 +0530

    Time: 189 ms (88.76%), Space: 21.2 MB (78.31%) - LeetHub

--- 0952-word-subsets/0952-word-subsets.py
+++ 0952-word-subsets/0952-word-subsets.py
@@ -1,30 +1,35 @@
 class Solution:
     def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
+        # Surely a brute force would take O(n)*O(n)*O(10) > 10^8 so TLE.
+        # A nice trick is to take all strings of words2 first and make
+        # a count map which will haveper word max occurance of each character.
+        # Ex. If we have ["lee", "lloc"]. So, surely to be universal, a string
+        # in words1 must have atleast 2 'l', atleast 2 'e' etc.
+
+        charFreq = [0]*26
+
+        for word in words2:
+            currwordFreq = [0]*26 
+            for ch in word:
+                chIdx = ord(ch) - ord('a')
+                currwordFreq[chIdx] += 1
+                charFreq[chIdx] = max(charFreq[chIdx], currwordFreq[chIdx])
         
-        target = {}
-        for targetWord in words2:
-            toAdd = {}
-            for letter in targetWord:
-                if letter not in toAdd:
-                    toAdd[letter] = 1
-                else:
-                    toAdd[letter] += 1
-            
-            for letter, occur in toAdd.items():
-                if letter in target:
-                    target[letter] = max(occur, target[letter])
-                else:
-                    target[letter] = occur
-        
-        ret = []
-        for a in words1:
-            toInclude = True
-            for key in target:
-                if a.count(key) < target[key]:
-                    toInclude = False
+        print(charFreq)
+        ans = []
+
+        for word in words1:
+            currwordFreq = [0]*26
+            for ch in word:
+                chIdx = ord(ch) - ord('a')
+                currwordFreq[chIdx] += 1
+
+            isUniversal = True
+            for i in range(26):
+                if(currwordFreq[i] < charFreq[i]):
+                    isUniversal = False
                     break
-            if toInclude:
-                ret.append(a)
-        return ret
-        
-        
+
+            if(isUniversal): ans.append(word)
+
+        return ans
