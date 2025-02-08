9af561e3a3d05bf046f0dbbcb696d091c651e896
Pritam723 <pritam.dutta723@gmail.com>
2025-02-08 22:54:17 +0530

    Time: 74 ms (56.74%), Space: 63.6 MB (26.28%) - LeetHub

--- 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.py
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.py
@@ -1,13 +1,47 @@
 class Solution:
     def queryResults(self, limit: int, queries: List[List[int]]) -> List[int]:
-        ball, color, ans, distinct = {}, {}, [], 0
-        for pos, c in queries:
-            if pos in ball:
-                color[ball[pos]] -= 1
-                if color[ball[pos]] == 0:
-                    distinct -= 1
-            ball[pos] = c
-            color[c] = color.get(c, 0) + 1
-            if color[c] == 1: distinct += 1
-            ans.append(distinct)
+        # O(limit) gives TLE.
+        n = len(queries)
+        
+        indexVisit = {}
+        colorFreq = {}
+
+        distinctColors = 0
+
+        ans = [0]*n
+
+        for i in range(n):
+            idx = queries[i][0]
+            color = queries[i][1]
+
+            if(indexVisit.get(idx) is None):
+                
+                # Color it first
+                indexVisit[idx] = color
+
+                if((colorFreq.get(color) is None) or (colorFreq[color] == 0)):
+                    colorFreq[color] = 1
+                    distinctColors = distinctColors + 1
+                else:
+                    colorFreq[color] = colorFreq[color] + 1
+                    # But do not increase distinctColors.
+            else:
+                # So, we are coloring the index again. Remove the old color first.
+                oldColor = indexVisit[idx]
+                # Color it with new.
+                indexVisit[idx] = color
+
+                colorFreq[oldColor] = colorFreq[oldColor] - 1
+                if(colorFreq[oldColor] == 0):  
+                    distinctColors = distinctColors - 1
+                
+                if((colorFreq.get(color) is None) or (colorFreq[color] == 0)):
+                    colorFreq[color] = 1
+                    distinctColors = distinctColors + 1
+                else:
+                    colorFreq[color] = colorFreq[color] + 1
+            
+            ans[i] = distinctColors
+
         return ans
+
