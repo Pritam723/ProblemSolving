3665cccfbaee409705434437e4aa8ac1e4ecf8b8
Pritam723 <pritam.dutta723@gmail.com>
2025-02-22 23:25:59 +0530

    Time: 10 ms (17.51%), Space: 22.4 MB (27.13%) - LeetHub

---
+++ 1093-recover-a-tree-from-preorder-traversal/1093-recover-a-tree-from-preorder-traversal.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int i=0;
+    TreeNode* recoverFromPreorder(string &S,int d)
+    {
+        int nextDigitPos=S.find_first_of("1234567890",i);                          // Get index of the first number after i.
+        if(nextDigitPos-i!=d)                                                      // If number of '-' in between != depth return NULL
+            return NULL;
+        int nextDashPos=S.find("-",nextDigitPos);                                  // Get the index of the next '-'
+        int rootValue=stoi(S.substr(nextDigitPos,nextDashPos-nextDigitPos));
+        TreeNode* root=new TreeNode(rootValue);                                    // Create the root with the node's value
+        i=nextDashPos;                                                             // Move index forward for future recursions.
+        root->left=recoverFromPreorder(S,d+1);                                     // Create left subtree
+        root->right=recoverFromPreorder(S,d+1);                                    // Create right subtree
+        return root;
+    }
+    TreeNode* recoverFromPreorder(string S)
+    {
+        return recoverFromPreorder(S,0);
+    }
+};
